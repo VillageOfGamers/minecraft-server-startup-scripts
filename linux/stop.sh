9a59292548980092417a1faaf4b965424309dc46
Giantvince1 <giantvince1@protonmail.com>
2023-05-14 19:51:56 -0500

    Update start_nointeract.sh and stop.sh to include proper logic surrounding PID detection and exit status detection, as well as provide a FIFO shell to the server's own input/output system. This eliminates reliability on mcrcon entirely, as the stop command can be read into the FIFO and get sent to the server that way.

--- linux/stop.sh
+++ linux/stop.sh
@@ -6,4 +6,11 @@
 # This allows the following script to be used for more than just sending stop to the Minecraft server without an interactive shell.
 rconpath="/usr/local/bin/mcrcon"
 cmd="stop"
+<<<<<<< HEAD
 $rconpath -H localhost -P 25575 -p "supersecretpasswordhere" $cmd
+=======
+pid=`cat /bulk/Minecraft/.pid`
+$rconpath -H localhost -P 25575 -p "Giantvince1" $cmd
+wait $pid
+sleep 10
+>>>>>>> 0fa9501 (Update start_nointeract.sh and stop.sh to include proper logic surrounding PID detection and exit status detection, as well as provide a FIFO shell to the server's own input/output system. This eliminates reliability on mcrcon entirely, as the stop command can be read into the FIFO and get sent to the server that way.)
